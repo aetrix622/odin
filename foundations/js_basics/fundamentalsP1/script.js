944ee295eec126f79732adb8f6268a892858926c
Joe Rochinski <127460332+aetrix622@users.noreply.github.com>
2023-11-24 11:37:21 -0500

    added project_landing_page and js fundamentals P1 to master odin repo

---
+++ foundations/js_basics/fundamentalsP1/script.js
@@ -0,0 +1,22 @@
+// ASSIGNMENT 
+// 1. add 2 numbers together
+console.log(23 + 97);
+
+// 2. add a sequence of 6 numbers together
+console.log(1 + 2 + 3 + 4 + 5 + 6);
+
+// 3. print the solution to (4 + 6 + 9) / 77 (should equal ~0.24675)
+console.log((4+6+9)/77);
+
+// 4. Let's use variables...
+let a = 10;
+console.log(a);
+console.log(9 * a);
+let b = 7 * a;
+console.log(b);
+
+// 5. try this sequence...
+const MAX = 57;
+let actual = MAX - 13;
+let percentage = actual / MAX;
+console.log(percentage); // should be ~.7719
