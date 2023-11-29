6d50448b0c32c4ec1c888a68d774e2afb95d5f13
Joe Rochinski <127460332+aetrix622@users.noreply.github.com>
2023-11-28 23:00:39 -0500

    complete exercise 05_sumAll

--- foundations/js_basics/javascript-exercises-main/05_sumAll/sumAll.js
+++ foundations/js_basics/javascript-exercises-main/05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+// Implement a function that takes 2 integers and returns the sum of every number between(and including) them:
 
+const sumAll = function(a, b) {
+    if (typeof a !== "number" || typeof b !== "number") return "ERROR";
+    if (a < 0 || b < 0) return "ERROR";
+    if (a > b) {
+        return sumAll(b, a);
+    }
+    let sum = 0;
+    for (i = a; i <= b; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
