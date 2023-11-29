8185e11f4528389baa5b06c13d272d51a81ed6fa
Joe Rochinski <127460332+aetrix622@users.noreply.github.com>
2023-11-28 21:36:02 -0500

    complete javascript-exercises 02_repeatstring

--- foundations/js_basics/javascript-exercises-main/02_repeatString/repeatString.js
+++ foundations/js_basics/javascript-exercises-main/02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(stringToRepeat, count) {
+    if (count < 0) return "ERROR";
+    let assembledString = "";
+    for (let i = 0; i < count; i++) {
+        assembledString += stringToRepeat;
+    }
+    return assembledString;
 };
 
+debugger;
 // Do not edit below this line
 module.exports = repeatString;
