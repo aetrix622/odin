4afabbda22e1f6f2fb2295c1f503e239b2cee1ff
Joe Rochinski <127460332+aetrix622@users.noreply.github.com>
2023-11-28 22:00:52 -0500

    complete exercise 03_reverseString

--- foundations/js_basics/javascript-exercises-main/03_reverseString/reverseString.js
+++ foundations/js_basics/javascript-exercises-main/03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(strForward) {
+    // convert arg into character array, reverse it, then reassemble
+    let strReverse = strForward.split("").reverse().join("");
+    return strReverse;
 };
 
+debugger;
 // Do not edit below this line
 module.exports = reverseString;
