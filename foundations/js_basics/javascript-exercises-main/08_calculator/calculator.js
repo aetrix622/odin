0f4dd02ed29ffb5f2a12ffc3245a97ba1850c9c7
Joe Rochinski <127460332+aetrix622@users.noreply.github.com>
2023-12-10 23:03:38 -0600

    completed 08_calculator

--- foundations/js_basics/javascript-exercises-main/08_calculator/calculator.js
+++ foundations/js_basics/javascript-exercises-main/08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let arraySum = 0;
+  arr.forEach(element => {
+    arraySum += element;
+  });
+  return arraySum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let arrayProduct = 1;
+  arr.forEach(element => {
+    arrayProduct *= element;
+  });
+  return arrayProduct;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	let result = 1;
+  for (let i = 1; i <= exponent; i++) {
+    result *= base;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n === 0) {
+    return 1;
+  } else {
+    result = n;
+    while (n > 1) {
+      result *= --n;
+    }
+  }
+  return result;
 };
 
 // Do not edit below this line
