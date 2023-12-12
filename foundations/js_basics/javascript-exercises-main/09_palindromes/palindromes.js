ef85aca1119f82c4f3886448ffac793a77016f75
Joe Rochinski <127460332+aetrix622@users.noreply.github.com>
2023-12-11 22:22:10 -0500

    completed exercise 09_palindromes

--- foundations/js_basics/javascript-exercises-main/09_palindromes/palindromes.js
+++ foundations/js_basics/javascript-exercises-main/09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (testString) {
+    testString = testString.toUpperCase().replace(/[^A-Z0-9]/g, "");
+    return testString == testString.split("").reverse().join("");
 };
 
 // Do not edit below this line
