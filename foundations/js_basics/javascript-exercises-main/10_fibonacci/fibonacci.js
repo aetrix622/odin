1553e2a5620651e2cb908b0309397a09f894a419
Joe Rochinski <127460332+aetrix622@users.noreply.github.com>
2023-12-11 22:53:20 -0500

    completed 10_fibonacci

--- foundations/js_basics/javascript-exercises-main/10_fibonacci/fibonacci.js
+++ foundations/js_basics/javascript-exercises-main/10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 1) {return "OOPS"};
+    let index = 1;
+    let numbers = [0, 1];
+    while (index < n) {
+        numbers.push(numbers[0] + numbers[1]);
+        numbers.shift();
+        index++
+    }
+    return numbers[1];
 };
 
 // Do not edit below this line
