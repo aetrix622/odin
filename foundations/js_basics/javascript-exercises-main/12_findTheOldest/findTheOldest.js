08583047d6b73d9c3b554cb326c0fa219c9a1aac
Aetrix622 <127460332+aetrix622@users.noreply.github.com>
2023-12-13 12:15:05 -0500

    completed 12_findTheOldest

--- foundations/js_basics/javascript-exercises-main/12_findTheOldest/findTheOldest.js
+++ foundations/js_basics/javascript-exercises-main/12_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleList) {
+    peopleList.forEach(a => {
+        const date1 = a.yearOfBirth;
+        let date2 = (new Date()).getFullYear();
+        if ("yearOfDeath" in a) {
+            date2 = a.yearOfDeath;
+        } 
+        a.age = date2 - date1;
+    });
 
+    peopleList.sort((a,b) => {
+        if (a.age > b.age) return -1;
+        if (a.age < b.age) return 1;
+    });
+
+    console.log("Oldest: " + peopleList[0].name);
+    return peopleList[0];
 };
 
+debugger;
 // Do not edit below this line
 module.exports = findTheOldest;
