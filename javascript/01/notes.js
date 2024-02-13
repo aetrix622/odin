a8837cae370cb627f894b21631c2f6035d9383c0
Joe Rochinski <127460332+aetrix622@users.noreply.github.com>
2024-02-13 16:26:13 -0500

    append prototypal inheretance notes

--- javascript/01/notes.js
+++ javascript/01/notes.js
@@ -87,23 +87,40 @@ const myObject = {
     player1.sayName(); // logs 'steve'
     player2.sayName(); // logs 'also steve'
 
-// EXERCISE: Book objects
-// Write a constructor for making book objects. Objects should contain title,
-// author, number of pages, and whether or not you've read the book.
+// EXERCISE: Book objects - refer to books.js
 
-    function Book(title, author, pages, read) {
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.read = read;
+// Accessing an object prototype
 
-        this.info = function() {
-            return `${title} by ${author}, ${pages} pages, ${read ? 'read' : 'not read yet'}`;
-        };
-    }
+    Object.getPrototypeOf(player1) === Player.prototype; // returns true
+    Object.getPrototypeOf(player2) === Player.prototype; // also returns true
+    // this because all objects made using the constructor share the same prototype. in fact:
+    Object.getPrototypeOf(player1) === Object.getPrototypeOf(player2); // is also true
 
-    const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
-    console.log(theHobbit.info());
-    
+// Why define properties and functions on the object prototype?
+    // 1: Do this when the property or function is the same for all objects to save memory
+    // 2: Prototypal inheretance reasons
+
+// Visualizing prototypal inheritance:
+    // Player.prototype.__proto__
+    Object.getPrototypeOf(Player.prototype) === Object.prototype; // TRUE
+    // we see that the prototype of the prototype of Player is the object prototype
+    // its prototype-ception
+
+    player1.valueOf(); // Output: Object { name: "steve", marker: "X", sayName: sayName() }
+    // Where did valueOf() come from? we didn't define it. Instead, it was inherited from Object.prototype.
+
+    // to see this in another light:
+    player1.hasOwnProperty('valueOf'); // FALSE because the Player constructor does not define valueOf
+    Object.prototype.hasOwnProperty('valueOf'); // TRUE
+    Object.prototype.hasOwnProperty('hasOwnProperty'); // TRUE (double-OMGWTF360noscope-prototype-ception)
+
+    // Final notes:
+    // 1. Every prototype inherits from Object.prototype by default
+    // 2. Every object's Object.getPrototypeOf() value can only be one unique prototype object.
+
+// Recommended Method for prototypal inheritance - refer to inheritance.js
+
+
+
 
 
